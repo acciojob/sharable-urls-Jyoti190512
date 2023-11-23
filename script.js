ed42c74fcc2c29d9f9dc37cdf63e842ff2b057dd
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2023-11-23 09:46:34 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // your code here
+function updateUrl() {
+        var name = encodeURIComponent(document.getElementById("name").value);
+        var year = encodeURIComponent(document.getElementById("year").value);
+
+        var url = "https://localhost:8080/?name=" + name + "&year=" + year;
+        document.getElementById("url").textContent = url;}
